4d6b10a9f6bd6b61bd7428a14484097149a2a109
brett <acodercalledcole@gmail.com>
2020-08-23 20:40:34 -0400

    component layout and created input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Column } from './Column';
+import { Card } from './Card';
 import { AppContainer } from './styles';
+import { AddNewItem } from './AddNewItem';
 
-function App() {
+const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="LearnTypescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
 }
